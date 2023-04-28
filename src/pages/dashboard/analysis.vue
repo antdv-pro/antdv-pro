11fe5fd3755940634595e48e0e413316e6056b04
aibayanyu20 <aibayanyu@qq.com>
2023-04-28 13:37:41 +0800

    fix: change layout background

--- src/pages/dashboard/analysis.vue
+++ src/pages/dashboard/analysis.vue
@@ -3,7 +3,8 @@
 
 <template>
   <div p-2>
-    <div h-500px bg-red class="w-100%" />
-    <div h-500px bg-green class="w-100%" />
+    <!-- <div h-500px bg-red class="w-100%" /> -->
+    <!-- <div h-500px bg-green class="w-100%" /> -->
+    Analysis
   </div>
 </template>
