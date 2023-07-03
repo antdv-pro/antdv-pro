5a702634bffd83e2d42712e37f095ca3e15d2245
aibayanyu20 <aibayanyu@qq.com>
2023-07-03 18:24:48 +0800

    fix: menu title error

--- src/locales/lang/global/zh-CN.ts
+++ src/locales/lang/global/zh-CN.ts
@@ -80,7 +80,7 @@ export default {
   'menu.exception.403': '403',
   'menu.exception.404': '404',
   'menu.exception.500': '500',
-  'menu.form': '表单页',
+  'menu.form.title': '表单页',
   'menu.form.basic-form': '基础表单',
   'menu.form.step-form': '分步表单',
   'menu.form.step-form.info': '分步表单（填写转账信息）',
