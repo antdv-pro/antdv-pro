e85713572270c1ddda04fdeaaffc755f3f3ebe2a
adekang <adekang@163.com>
2023-08-11 18:26:46 +0800

    feat: add exception page

---
+++ src/pages/exception/500.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+const router = useRouter()
+const back = () => {
+  router.replace({
+    path: '/',
+  })
+}
+</script>
+
+<template>
+  <a-result status="500" title="500" sub-title="Sorry, the server is reporting an error.">
+    <template #extra>
+      <a-button type="primary" @click="back">
+        Back Home
+      </a-button>
+    </template>
+  </a-result>
+</template>
