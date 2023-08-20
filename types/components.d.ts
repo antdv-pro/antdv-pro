1086a8dce82536f60fd44ee2aa43c824d4d0e8b7
aibayanyu <aibayanyu@qq.com>
2023-08-20 14:26:06 +0800

    fix: fix qrcode

--- types/components.d.ts
+++ types/components.d.ts
@@ -39,6 +39,7 @@ declare module 'vue' {
     AMenuDivider: typeof import('ant-design-vue/es')['MenuDivider']
     AMenuItem: typeof import('ant-design-vue/es')['MenuItem']
     AModal: typeof import('ant-design-vue/es')['Modal']
+    AQrcode: typeof import('ant-design-vue/es')['QRCode']
     ARadio: typeof import('ant-design-vue/es')['Radio']
     ARadioButton: typeof import('ant-design-vue/es')['RadioButton']
     ARadioGroup: typeof import('ant-design-vue/es')['RadioGroup']
