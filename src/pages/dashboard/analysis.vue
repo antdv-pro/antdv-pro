b0ceb48e724b79ec4837a103cdf4640f82122491
杜天宇 <17862705909@163.com>
2023-09-07 10:32:57 +0800

    feat: complete number-info and top-search demo

--- src/pages/dashboard/analysis.vue
+++ src/pages/dashboard/analysis.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import IntroduceRow from '~/pages/dashboard/introduce-row.vue'
 import SalesCard from '~/pages/dashboard/sales-card.vue'
+import TopSearch from '~/pages/dashboard/components/top-search.vue'
 
 const loading = ref(false)
 
@@ -16,5 +17,27 @@ const visitData = ref([])
     <Suspense :fallback="null">
       <SalesCard />
     </Suspense>
+
+    <a-row
+      :gutter="24"
+      :style="{ marginTop: '24px' }"
+    >
+      <a-col :xl="12" :lg="24" :md="24" :sm="24" :xs="24">
+        <Suspense :fallback="null">
+          <TopSearch />
+        </Suspense>
+      </a-col>
+      <a-col :xl="12" :lg="24" :md="24" :sm="24" :xs="24">
+        <Suspense :fallback="null">
+          <!--          <ProportionSales -->
+          <!--            dropdown-group="{dropdownGroup}" -->
+          <!--            sales-type="{salesType}" -->
+          <!--            loading="{loading}" -->
+          <!--            sales-pie-data="{salesPieData" || []} -->
+          <!--            handle-change-sales-type="{handleChangeSalesType}" -->
+          <!--          /> -->
+        </Suspense>
+      </a-col>
+    </a-row>
   </page-container>
 </template>
