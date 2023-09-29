2201119968b92b8e275804c95133202c55d4b90a
w <674801810@qq.com>
2023-09-29 14:09:04 +0800

    feat: add svg demo pages

---
+++ src/pages/feat/icon/index.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div>
+    <a-card title="unplugin-icons 使用">
+      <a-card-grid style="width: 25%; text-align: center">
+        <i-assets-svg:vue />
+      </a-card-grid>
+
+      <a-card-grid style="width: 25%; text-align: center">
+        <i-material-symbols:123-sharp />
+      </a-card-grid>
+      <a-card-grid style="width: 25%; text-align: center">
+        <i-material-symbols:123-sharp />
+      </a-card-grid>
+    </a-card>
+  </div>
+</template>
+
+<style scoped></style>
