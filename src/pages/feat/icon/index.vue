1a7ad85e07896a1ba587d795f9bf3a752e72d390
w <674801810@qq.com>
2023-10-03 12:47:56 +0800

    feat(svg): 打包内存溢出问题 uncoss 动态加载图片说明补充

--- src/pages/feat/icon/index.vue
+++ src/pages/feat/icon/index.vue
@@ -1,15 +1,46 @@
 <script setup lang="ts">
-// @unocss-include
 import { icons as vscodeIcons } from '@iconify-json/vscode-icons'
 defineOptions({
   name: 'FeatIcon',
 })
 
-const icons = computed(() => {
+const dynamicIcons = computed(() => {
   const iconsNames = Object.keys(vscodeIcons.icons)
-  return iconsNames.slice(0, 21).map(icon => `i-vscode-icons:${icon}`)
+  return iconsNames.slice(0, 21).map(icon => `i-${vscodeIcons.prefix}:${icon}`)
 })
-console.log(icons)
+
+const icons = [
+  'i-vscode-icons:file-type-antlr',
+  'i-vscode-icons:file-type-anyscript',
+  'i-vscode-icons:file-type-apache',
+  'i-vscode-icons:file-type-apex',
+  'i-vscode-icons:file-type-api-extractor',
+  'i-vscode-icons:file-type-apib',
+  'i-vscode-icons:file-type-apib2',
+  'i-vscode-icons:file-type-apl',
+  'i-vscode-icons:file-type-applescript',
+  'i-vscode-icons:file-type-appscript',
+  'i-vscode-icons:file-type-appsemble',
+  'i-vscode-icons:file-type-appveyor',
+  'i-vscode-icons:file-type-arduino',
+  'i-vscode-icons:file-type-asciidoc',
+  'i-vscode-icons:file-type-asp',
+  'i-vscode-icons:file-type-aspx',
+  'i-vscode-icons:file-type-assembly',
+  'i-vscode-icons:file-type-astro',
+  'i-vscode-icons:file-type-astroconfig',
+  'i-vscode-icons:file-type-ats',
+  'i-vscode-icons:file-type-audio',
+  'i-vscode-icons:file-type-aurelia',
+  'i-vscode-icons:file-type-autohotkey',
+  'i-vscode-icons:file-type-autoit',
+  'i-vscode-icons:file-type-avif',
+  'i-vscode-icons:file-type-avro',
+  'i-vscode-icons:file-type-awk',
+  'i-vscode-icons:file-type-aws',
+  'i-vscode-icons:file-type-azure',
+  'i-vscode-icons:file-type-azurepipelines',
+]
 </script>
 
 <template>
@@ -36,14 +67,20 @@ console.log(icons)
       </a-card-grid>
       <a-card-grid style="width: 25%; text-align: center">
         <div class="flex justify-between items-center">
-          <span>使用iconify 图标集</span>
+          <span>使用iconify 动态图标集</span>
 
           <div class="i-vscode-icons:file-type-light-pnpm?mask text-red-300" />
         </div>
       </a-card-grid>
     </a-card>
 
-    <a-card title="iconify-json/vscode-icons 图标集" class="mt5">
+    <a-card title="动态加载 iconify-json/vscode-icons 图标集" class="mt5">
+      <a-card-grid v-for="(icon, idx) in dynamicIcons" :key="idx" style="width: 25%">
+        <div :class="[icon]" />
+      </a-card-grid>
+    </a-card>
+
+    <a-card title="批量固定引入 iconify-json/vscode-icons 图标集" class="mt5">
       <a-card-grid v-for="(icon, idx) in icons" :key="idx" style="width: 25%">
         <div :class="[icon]" />
       </a-card-grid>
--- unocss.config.ts
+++ unocss.config.ts
@@ -15,10 +15,16 @@ import { FileSystemIconLoader } from '@iconify/utils/lib/loader/node-loaders'
 import { icons as vscodeIcons } from '@iconify-json/vscode-icons'
 import antdUnoTheme from './themes/antd-uno-theme.json'
 
-// 由于 UnoCSS 在构建时使用静态提取，在编译时我们不可能知道所有实用程序的组合 https://unocss.dev/guide/extracting#safelist
+/**
+ * 如果你想做动态加载 svg 显然 unocss 是不明智的，动态像svg demo 中通过计算属性等其他情况拼接
+ * 由于 UnoCSS 在构建时使用静态提取，在编译时我们不可能知道所有实用程序的组合 因此需要提前注册
+ * https://unocss.dev/guide/extracting#safelist
+ * 这样带来的问题会将包体积将会变大
+ * 如果你并不是需要动态加载svg 记住请移除这个配置，包整体测算导入一个vscodeIcons 图标集的包多出6MB
+ */
 const safeVscodeIconsList = Object.keys(vscodeIcons.icons).map(
   icon => `i-${vscodeIcons.prefix}:${icon}`,
-)
+).splice(0, 20)
 
 export default defineConfig({
   safelist: [...safeVscodeIconsList],
