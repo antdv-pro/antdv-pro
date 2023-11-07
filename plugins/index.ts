d1f82e4ab32877b5aea9ec019bbd7859f109a2ab
Kirk Lin <linkirk@163.com>
2023-11-07 08:53:41 +0800

    chore: unified build output directory #118

--- plugins/index.ts
+++ plugins/index.ts
@@ -6,7 +6,7 @@ import GenerateConfig from 'unplugin-config/vite'
 import Components from 'unplugin-vue-components/vite'
 import Unocss from 'unocss/vite'
 import AntdvResolver from 'antdv-component-resolver'
-import { APP_NAME, GLOB_CONFIG_FILE_NAME, OUTPUT_DIR } from './constants'
+import { GLOB_CONFIG_FILE_NAME, OUTPUT_DIR } from './constants'
 import { viteBuildInfo } from './vite-build-info'
 
 export function createVitePlugins(env: Record<string, string>) {
@@ -31,7 +31,7 @@ export function createVitePlugins(env: Record<string, string>) {
     }),
     // https://github.com/kirklin/unplugin-config
     GenerateConfig({
-      appName: APP_NAME,
+      appName: env.VITE_GLOB_APP_TITLE,
       configFile: {
         generate: true,
         fileName: GLOB_CONFIG_FILE_NAME,
