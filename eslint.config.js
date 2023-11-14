baa7747b3c6601f6aec9be62fbcdf2a5a8632626
aibayanyu20 <aibayanyu@qq.com>
2023-11-14 11:07:39 +0800

    chore: delete eslintignore replace eslint.config

--- eslint.config.js
+++ eslint.config.js
@@ -7,11 +7,14 @@ export default mist({
   gitignore: true,
   markdown: true,
   ignores: [
+    'types/auto-imports.d.ts',
+    'types/components.d.ts',
+    'public',
     'tsconfig.*.json',
     'tsconfig.json',
   ],
 }, {
   rules: {
-    'no-console': 'off',
+    'no-console': 0,
   },
 })
