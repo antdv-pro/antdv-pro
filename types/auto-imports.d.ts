ab64028ec172f5b45f56f7382066b88cd888d412
wangqifan <wangqifan@wxdata.cn>
2024-01-12 18:35:19 +0800

    feat: add table-query hook for crud table

--- types/auto-imports.d.ts
+++ types/auto-imports.d.ts
@@ -278,6 +278,7 @@ declare global {
   const useStyleTag: typeof import('@vueuse/core')['useStyleTag']
   const useSupported: typeof import('@vueuse/core')['useSupported']
   const useSwipe: typeof import('@vueuse/core')['useSwipe']
+  const useTableQuery: typeof import('../src/composables/table-query')['useTableQuery']
   const useTemplateRefsList: typeof import('@vueuse/core')['useTemplateRefsList']
   const useTextDirection: typeof import('@vueuse/core')['useTextDirection']
   const useTextSelection: typeof import('@vueuse/core')['useTextSelection']
