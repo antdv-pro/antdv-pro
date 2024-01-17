8db54258f29179f143bbd4c753273f5e8e7d420e
kaivanwong <kaivanwong@outlook.com>
2024-01-17 10:59:53 +0800

    revert: cancel siplitMenu in header logo component

--- src/layouts/components/global-header/global-header-logo.vue
+++ src/layouts/components/global-header/global-header-logo.vue
@@ -1,10 +1,10 @@
 <script setup lang="ts">
 import { useLayoutState } from '../../basic-layout/context'
 
-const { logo, title, layout, isMobile, splitMenus } = useLayoutState()
+const { logo, title, layout, isMobile } = useLayoutState()
 const cls = computed(() => ({
   'ant-pro-global-header-logo': layout.value === 'mix' || isMobile.value,
-  'ant-pro-top-nav-header-logo': (layout.value === 'top' || splitMenus.value) && !isMobile.value,
+  'ant-pro-top-nav-header-logo': layout.value === 'top' && !isMobile.value,
 }))
 </script>
 
